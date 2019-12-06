55ee0a4af1844df061e9e71fc90e18581c4f9be7
Johannes Christ <jc@jchri.st>
2019-12-06 17:41:31 +0100

    Skip subprocess test in CI.

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,6 +1,12 @@
+import os
 import subprocess
 
+import pytest
 
+
+@pytest.mark.skipif(
+    os.getenv('CI') == 'true', reason="docker images disallow replication connections"
+)
 def test_invoke_via_entry_point():
     # how the hell does coverage parsing work here?
     # this is pure magic
