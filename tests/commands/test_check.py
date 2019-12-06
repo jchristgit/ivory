868c5d0aad1ee6216e62833f14d0dc63a6aa4507
Johannes Christ <jc@jchri.st>
2019-12-06 17:44:22 +0100

    Skip check test in CI.

--- tests/commands/test_check.py
+++ tests/commands/test_check.py
@@ -6,6 +6,9 @@ from ivory.commands import check
 
 
 @pytest.mark.asyncio
+@pytest.mark.skipif(
+    os.getenv('CI') == 'true', reason="docker images disallow replication connections"
+)
 async def test_run_reports_rc_0(cli_parser: argparse.ArgumentParser) -> None:
     args = cli_parser.parse_args(['check'])
     rc = await check.run(args)
