311d6bdc962aa4f3c0bd0c0d37cf9923329b0b49
Johannes Christ <jc@jchri.st>
2022-12-27 23:23:35 +0100

    Print diff as string, not number

--- ivory/commands/replication/status.py
+++ ivory/commands/replication/status.py
@@ -86,7 +86,7 @@ async def run(args: argparse.Namespace) -> int:
         log.info("LSN matches.")
     else:
         log.warning(
-            "Source is at LSN %x, standby at %x (diff %d).",
+            "Source is at LSN %x, standby at %x (diff %s).",
             my_lsn,
             replication_stats['flush_lsn'],
             my_lsn - replication_stats['flush_lsn'],
